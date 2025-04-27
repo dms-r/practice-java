6d506020dfdbd1906a86664c1e09bb1024c98786
dms-r <dms@mint>
2025-04-27 10:10:13 +0700

    add tyoe data number

---
+++ TypeDataNumber.java
@@ -0,0 +1,24 @@
+public class TypeDataNumber
+{   
+	public static void main (String[] args)
+	{
+        byte typeByte = 100;
+        short typeShort = 1_000;
+        int typeInt = 1_000_000;
+        long typeLong = 1_000_000L;
+
+		System.out.println(typeByte);
+		System.out.println(typeShort);
+		System.out.println(typeInt);
+		System.out.println(typeLong);
+
+        float typeFloat = -1.8F;
+        double typeDouble = 1.000000000000000000000000000009F;
+        
+		System.out.println(typeFloat);
+		System.out.println(typeDouble);
+
+
+	}
+}
+
