34a28141c0134fc56813beec6b7486e82cdfb388
dms-r <dms@mint>
2025-04-29 14:21:42 +0700

    add type data str

---
+++ TypeDataString.java
@@ -0,0 +1,13 @@
+public class TypeDataString
+{
+
+  public static void main(String[] args) {
+
+    String firstName = "Dyl ";
+    String lastName = "Zen";
+
+    System.out.print(firstName);
+    System.out.print(lastName);
+
+  }
+}
--- helloWorld.java
+++ helloWorld.java
@@ -2,8 +2,6 @@ public class HelloWorld
 {   
 	public static void main (String[] args)
 	{
-		// Click on submit to view the output
 		System.out.println("\nHello World!");
 	}
 }
-
